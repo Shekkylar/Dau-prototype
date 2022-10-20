0ae6a050a1179f0e6b7d82d12ee7e772c1ba2c1d
shekkylar18 <shekkylar.freightify.com>
2022-10-20 12:23:13 +0530

    changes

--- src/repoistory/tableDatas.ts
+++ src/repoistory/tableDatas.ts
@@ -6,7 +6,31 @@ export const tabledatas = async (filter:any,skip:any,limit:any)=>{
     var z: number = +limit
     console.log(y)
     console.log(z)
-       const res = await col.find(filter).skip(y*z).limit(z)
+       const res = await col.find(filter,{
+        'meta.origin_port': 1,
+        'meta.destination_port': 1,
+        'data.via_port': 1,
+        'meta.load_type': 1,
+        'data.contract_number': 1,
+        'data.transit_time': 1,
+        'data.other_charges': 1,
+        'data.service_type': 1,
+        'data.inclusions': 1,
+        'data.if_applicable_charges': 1,
+        'data.remarks': 1,
+        'meta.start_date': 1,
+        'data.expiry': 1,
+        'data.via_pol': 1,
+        'data.via_pod': 1,
+        'data.cargo_type': 1,
+        'data.commodity': 1,
+        'data.charges.charge_id': 1,
+        'data.charges.charge_name': 1,
+        'data.charges.charge_cost': 1,
+        'data.charges.charge_basis': 1,
+        'data.charges.charge_currency': 1,
+        'data.charges.charge_description': 1
+      }).skip(y*z).limit(z)
        console.log(res)
        return res;
 } 
--- src/service/tableDatas.ts
+++ src/service/tableDatas.ts
@@ -60,7 +60,32 @@ export default class tableDatas{
     }
     else{
       const datas:any= await tabledatas(filter,skip,limit);
-      return datas;
+      const output=[]
+      for(let i=0;i<datas.length;i++){
+      let temp:{_id:any,origin_port:any,destination_port:any,via_port:any,load_type:any,contract_number:any,start_date:any,remarks:any,inclusion:any,expiry:any,other_charges:any,if_applicable_charges:any,via_pol:any,via_pod:any,service_type:any,cargo_type:any,commodity:any,charges:any}=
+      {
+        _id:datas[i]._id,
+        origin_port:datas[i].meta.origin_port,
+        destination_port:datas[i].meta.destination_port,
+        via_port:datas[i].data.via_port,
+        load_type:datas[i].meta.load_type,
+        contract_number:datas[i].data.contract_number,
+        start_date:datas[i].meta.start_date,
+        remarks:datas[i].data.remarks,
+        inclusion:datas[i].data.inclusions,
+        expiry:datas[i].data.expiry,
+        other_charges:datas[i].data.other_charges,
+        if_applicable_charges:datas[i].data.if_applicable_charges,
+        via_pod:datas[i].data.via_pod,
+        via_pol:datas[i].data.via_pol,
+        service_type:datas[i].data.service_type,
+        cargo_type:datas[i].data.cargo_type,
+        commodity:datas[i].data.commodity,
+        charges:datas[i].data.charges,
+      }
+        output.push(temp);
+    }
+      return output ;
     }
       
   }
